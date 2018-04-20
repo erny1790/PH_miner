87664c3e990b880e3c56df0d7571721fce35241e
Ernesto <erny1790@live.it>
2018-04-20 14:08:41 +0200

    Revert "Modified phminer.py to satisfy requirements in Issue #1, result are in csv file"
    This reverts commit 24c2656b733a7e2d0818ce3fbf7a499e94f8e176.

--- phminer.py
+++ phminer.py
@@ -3,40 +3,17 @@ import os
 import yaml
 from ph_py import ProductHuntClient
 from ph_py.error import ProductHuntError
-import csv
 
 config_file = 'credentials.yml'
+
 
 def run(key, secret, uri, token):
     phc = ProductHuntClient(key, secret, uri, token)
 
     # Example request
     try:
-        with open('popular_2018-04-20.csv', 'w', newline='', encoding='UTF8') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_ALL)
-            spamwriter.writerow(['Name', 'TagLine', 'Logo', 'URL', 'NumberOfUpvotes', 'NumberOfComments', 'HunterName'])
-
-            for post in phc.get_todays_posts():
-                print('Name: ', post.name)
-                print('Tagline: ', post.tagline)
-
-                logo=post.screenshot_url['850px']
-                print('Logo: ', logo)
-                print('URL: ', post.redirect_url)
-                print('number of upvotes: ', post.votes_count)
-                print('number of comments: ', post.comments_count)
-                print('Hunter name: ', post.user.name)
-
-
-                spamwriter.writerow([post.name, post.tagline, logo, post.redirect_url, post.votes_count, post.comments_count, post.user.name])
-
-                print('-------------------------------------------')
-                print('-------------------------------------------')
-
-
-
-
-
+        for post in phc.get_todays_posts():
+            print(post.name)
     except ProductHuntError as e:
         print(e.error_message)
         print(e.status_code)
